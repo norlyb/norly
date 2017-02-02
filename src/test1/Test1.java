b29370327632c33bee31ecc132dcb63db78cd1d4
Estudiante <Estudiante@10.203.3.195>
2017-02-02 15:57:40 -0500

    Hola Julian e.e

--- src/test1/Test1.java
+++ src/test1/Test1.java
@@ -15,7 +15,7 @@ public class Test1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo :v ");
+        System.out.println("Hola Julian ");
         // TODO code application logic here
     }
     
